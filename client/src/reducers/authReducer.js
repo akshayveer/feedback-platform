ea33b9089373ce04310f11d143750373c3a7fa8f
akshayveer <akshayveer096@gmail.com>
2017-11-09 22:24:47 +0530

    send state of authentication from auth reducer

--- client/src/reducers/authReducer.js
+++ client/src/reducers/authReducer.js
@@ -1,6 +1,11 @@
-export default function(state = {}, action) {
+import { FETCH_USER } from '../actions/types';
+
+export default function(state = null, action) {
   console.log(action);
   switch (action.type) {
+    case action.type == FETCH_USER:
+      return action.payload || false;
+      break;
     default:
       return state;
   }
