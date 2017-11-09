c6d011683fcfcf672e645883723a16342e597fd1
akshayveer <akshayveer096@gmail.com>
2017-11-09 22:39:28 +0530

    Pass on auth values to Header

--- client/src/reducers/authReducer.js
+++ client/src/reducers/authReducer.js
@@ -3,7 +3,7 @@ import { FETCH_USER } from '../actions/types';
 export default function(state = null, action) {
   console.log(action);
   switch (action.type) {
-    case action.type == FETCH_USER:
+    case FETCH_USER:
       return action.payload || false;
       break;
     default:
