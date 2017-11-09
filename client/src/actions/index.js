b65f335b0627526bec2b008c980f72ee62b3d2ab
akshayveer <akshayveer096@gmail.com>
2017-11-09 21:25:07 +0530

    Action creator for auth and redux thunk middleware

---
+++ client/src/actions/index.js
@@ -0,0 +1,21 @@
+import axios from 'axios';
+import { FETCH_USER } from './types';
+
+const fetchUser = () => {
+  return function(dispatch) {
+    axios
+      .get('/api/current_user')
+      .then(res => {
+        dispatch({
+          type: FETCH_USER,
+          payload: res
+        });
+      })
+      .catch(err => {
+        dispatch({
+          type: FETCH_USER,
+          payload: err
+        });
+      });
+  };
+};
