abea8b532a5e0524d5111f62b5e525f4ea0b09bd
akshayveer <akshayveer096@gmail.com>
2017-11-11 12:03:17 +0530

    Production routes

--- index.js
+++ index.js
@@ -25,5 +25,16 @@ app.use(passport.session());
 require('./routes/auth-routes')(app);
 require('./routes/billing-routes')(app);
 
+if (process.env.NODE_ENV === 'production') {
+  // Express will serve up production assests
+  app.use(express.static('client/build'));
+
+  // Express will serve index.html if it doesn't recognise route
+  const path = require('path');
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
