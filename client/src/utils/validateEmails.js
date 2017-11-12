622d2f817242fe980de6216069d7cc1b3a8bee98
akshayveer <akshayveer096@gmail.com>
2017-11-12 20:26:51 +0530

    add validate email function

---
+++ client/src/utils/validateEmails.js
@@ -0,0 +1,15 @@
+const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+export default emails => {
+  const invalidEmails = emails
+    .split(',')
+    .map(email => {
+      return email.trim();
+    })
+    .filter(email => {
+      return re.test(email) === false;
+    });
+  if (invalidEmails.length) {
+    return `These are invalid emails ${invalidEmails}`;
+  }
+};
