ef8eacb5e4cd5dbe6121600f3eacf4db308747f9
akshayveer <akshayveer096@gmail.com>
2017-11-13 20:09:33 +0530

    Refactor form labels to some const file to reuse in form review

--- client/src/components/surveys/SurveyFormReview.js
+++ client/src/components/surveys/SurveyFormReview.js
@@ -1,27 +1,22 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import _ from 'lodash';
+import formFields from './formFields';
 
-const SurveyFormReview = ({ title, subject, body, emails, onCancel }) => {
+const SurveyFormReview = ({ formValues, onCancel }) => {
+  const reviewFields = _.map(formFields, field => {
+    return (
+      <div key={field.name}>
+        <label>{field.label}</label>
+        <div>{formValues[field.name]}</div>
+      </div>
+    );
+  });
+
   return (
     <div>
       <h2>Please confirm your entries</h2>
-      <div>
-        <h5>Title</h5>
-        <p>{title}</p>
-      </div>
-      <div>
-        <h5>Subject</h5>
-        <p>{subject}</p>
-      </div>
-      <div>
-        <h5>Body</h5>
-        <p>{body}</p>
-      </div>
-      <div>
-        <h5>Recipients</h5>
-        <p>{emails}</p>
-      </div>
-
+      {reviewFields}
       <button className="yellow darken-3 btn-flat" onClick={onCancel}>
         Back
       </button>
@@ -30,6 +25,6 @@ const SurveyFormReview = ({ title, subject, body, emails, onCancel }) => {
 };
 
 function mapStateToProps(state) {
-  return state.form.surveyForm.values;
+  return { formValues: state.form.surveyForm.values };
 }
 export default connect(mapStateToProps, null)(SurveyFormReview);
