db66d53f3f3aa624751e228d9ffbe62ec1422562
akshayveer <akshayveer096@gmail.com>
2017-11-15 20:04:27 +0530

    create suveys route

--- routes/survey-routes.js
+++ routes/survey-routes.js
@@ -14,6 +14,13 @@ module.exports = app => {
     res.send('Thanks for providing valuable feedback');
   });
 
+  app.get('/api/surveys', requireLogin, async (req, res) => {
+    const surveys = await Survey.find({ _user: req.user.id }).select({
+      recipients: false
+    });
+    res.send(surveys);
+  });
+
   app.post('/api/surveys/webhooks', (req, res) => {
     const p = new Path('/api/surveys/:surveyId/:choice');
     _.chain(req.body)
